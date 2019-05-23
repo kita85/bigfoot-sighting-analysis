4e8c415b90cdf968510f137f952bcef27b25bcd9
kita85 <ndmaynard42@gmail.com>
2019-05-23 07:54:31 -0400

    Update bigfoot.php

--- bigfoot.php
+++ bigfoot.php
@@ -3,31 +3,32 @@
 //define server variables
 include 'conn.php';
 
-//Which function to call
+//Call the function if the ajax post is not empty. INCLUDE SANITATION!!!
 if(!empty($_POST)) {
-	$searchValue = $_POST['searchValue'];
-	charts($searchValue);
+	$searchValue = $_POST['searchValue']; //row to be queried 
+	$orderValue = $_POST['orderValue']; //which variable to order data by
+	$dirValue = $_POST['dirValue']; //ASC or DESC order by direction
+	$limitValue = $_POST['limitValue']; //limit results
+	charts($searchValue,$orderValue,$dirValue,$limitValue);
 }
 		
 
-		function charts ($searchValue) {
+		function charts ($searchValue,$orderValue,$dirValue,$limitValue) {
+				//Connect to the database
 				$conn = db();
-				//if ($searchValue === "year") {
-					//$sql = "SELECT `".$searchValue."`, COUNT(`".$searchValue."`), floor(year/10)*10 as count from `data` group by count;";
-					//$sql = "SELECT `".$searchValue."`, COUNT(`".$searchValue."`) AS `count` FROM `data` WHERE `".$searchValue."` IS NOT NULL GROUP BY `".$searchValue."` LIMIT 5;";
-				//} else {
-					$sql = "SELECT `".$searchValue."`, COUNT(`".$searchValue."`) AS `count` FROM `data` WHERE `".$searchValue."`!='' GROUP BY `".$searchValue."` ORDER BY `count` DESC LIMIT 10;";
-				//}
 				
-
+				//Reusable database query for all charts and tables
+				$sql = "SELECT `".$searchValue."`, COUNT(`".$searchValue."`) AS `count` FROM `data` WHERE `".$searchValue."`!='' GROUP BY `".$searchValue."` ORDER BY `".$orderValue."` ".$dirValue." LIMIT ".$limitValue.";";
 				$result = $conn->query($sql);
 				
+				//fetch results
 				if($result->num_rows > 0) {
 					while($row = $result->fetch_assoc()) {
+						//return results as json string
 						echo json_encode($row[$searchValue]).",";
 						echo json_encode($row['count']).",";
 					}
 				}
-				$conn->close();
-			}
+			$conn->close();
+		}
 ?>
